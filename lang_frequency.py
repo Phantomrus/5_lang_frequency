22bdc790d420060b5276eab7ef4cf4fccf7ac121
Phantomrus <phantomrus@gmail.com>
2016-10-07 16:56:38 +0300

    Добавлена переменная count_of_words
    Добавлена переменная count_of_words - количество самых частых слов, которое нужно выводить.

--- lang_frequency.py
+++ lang_frequency.py
@@ -10,13 +10,14 @@ def get_words(text):
     words = re.findall(r'\w+', text.lower())
     return collections.Counter(words)
 
-def print_ten_freq_words(words):
+def print_ten_freq_words(words, count_of_words):
     print('Ten most frequent words:')
-    for pair in words.most_common(10):
+    for pair in words.most_common(count_of_words):
         print(pair[0], pair[1]) # Выводим 10 самых часто встречающихся слов и количество их появлений
 
 if __name__ == '__main__':
+    count_of_words = 10
     file_path = input("Enter the path to the file: ")
     text = load_data(file_path)
     words = get_words(text)
-    print_ten_freq_words(words)
+    print_ten_freq_words(words, count_of_words)
