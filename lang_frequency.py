c06b5d6d0c8c47fc1553fd60b4f514559f4287aa
Phantomrus <phantomrus@gmail.com>
2016-10-07 16:52:59 +0300

    Вывод вынесен в отдельную функцию
    Вывод десяти самых частых слов вынесен в отдельную функцию print_ten_freq_words.

--- lang_frequency.py
+++ lang_frequency.py
@@ -6,18 +6,17 @@ def load_data(filepath):
         text_file= text_file.read()
     return text_file
 
-def get_most_frequent_words(text):
+def get_words(text):
     words = re.findall(r'\w+', text.lower())
-    freq_words = []  
-    print('\nTen most frequent words:')
-    for pair in collections.Counter(words).most_common(10):
-        freq_words.append([pair[0], pair[1]]) # Получаем 10 самых часто встречающихся слов и количество их появлений и добавляем в массив 
-    return freq_words
+    return collections.Counter(words)
 
+def print_ten_freq_words(words):
+    print('Ten most frequent words:')
+    for pair in words.most_common(10):
+        print(pair[0], pair[1]) # Выводим 10 самых часто встречающихся слов и количество их появлений
 
 if __name__ == '__main__':
     file_path = input("Enter the path to the file: ")
     text = load_data(file_path)
-    ten_freq_words = get_most_frequent_words(text)
-    for element in ten_freq_words:
-        print(element[0], element[1])
+    words = get_words(text)
+    print_ten_freq_words(words)
