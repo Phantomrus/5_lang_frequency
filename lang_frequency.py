72a9a5db5bcbe095224fccd351bbd6d6a5ce0e22
Phantomrus <phantomrus@gmail.com>
2016-10-10 08:13:40 +0300

    Добавлены более понятные комментарии
    Добавлен комментарий к переменной count_of_words.
    Обновлен комментарий к выводу самых часто встречающихся слов.

--- lang_frequency.py
+++ lang_frequency.py
@@ -13,10 +13,10 @@ def get_words(text):
 def print_ten_freq_words(words, count_of_words):
     print('Ten most frequent words:')
     for pair in words.most_common(count_of_words):
-        print(pair[0], pair[1]) # Выводим 10 самых часто встречающихся слов и количество их появлений
+        print(pair[0], pair[1]) # Выводим самые часто встречающиеся слова (количество содержится в переменной count_of_words) и количество их появлений.
 
 if __name__ == '__main__':
-    count_of_words = 10
+    count_of_words = 10 # Количество самых часто встречаемых слов, которое мы хотим вывести на экран.
     file_path = input("Enter the path to the file: ")
     text = load_data(file_path)
     words = get_words(text)
